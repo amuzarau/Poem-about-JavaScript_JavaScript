24afe966aa4a51934820d678d1f68c9081a23014
Andrei Muzarau <95071115+amuzarau@users.noreply.github.com>
2025-08-19 19:28:06 +0300

    Add files via upload

---
+++ poem_about_JavaScript.js
@@ -0,0 +1,14 @@
+// Two ways to write multiline strings:
+
+// 1.With escape sequence
+let poem = "Roses are red,\nViolets are blue, \nJavaScript is fun,\nAnd so are you."
+
+console.log(poem)
+
+// 2.With template literal 
+let poem = `Roses are red,
+Violets are blue,
+JavaScript is fun,
+And so are you.`
+
+console.log(poem)
